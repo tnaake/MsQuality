be378d1b861c5a6e6b92115a7abd8e0ceba0ef2c
tnaake <thomasnaake@gmx.de>
2022-09-16 16:29:59 +0200

    Update test_function_visualizations.R

--- tests/testthat/test_function_visualizations.R
+++ tests/testthat/test_function_visualizations.R
@@ -4,32 +4,6 @@ library(Spectra)
 fls <- dir(system.file("sciex", package = "msdata"), full.names = TRUE)
 spectra <- Spectra(fls, backend = MsBackendMzR())
 
-## create toy example data set (MsExperiment)
-library(msdata)
-library(MsExperiment)
-library(S4Vectors)
-mse <- MsExperiment()
-sd <- DataFrame(sample_id = c("QC1", "QC2"),
-    sample_name = c("QC Pool", "QC Pool"), injection_idx = c(1, 3))
-sampleData(mse) <- sd
-
-## define file names containing spectra data for the samples and
-## add them, along with other arbitrary files to the experiment
-fls <- dir(system.file("sciex", package = "msdata"), full.names = TRUE)
-experimentFiles(mse) <- MsExperimentFiles(
-    mzML_files = fls,
-    annotations = "internal_standards.txt")
-## link samples to data files: first sample to first file in "mzML_files",
-## second sample to second file in "mzML_files"
-mse <- linkSampleData(mse, with = "experimentFiles.mzML_files",
-                      sampleIndex = c(1, 2), withIndex = c(1, 2))
-mse <- linkSampleData(mse, with = "experimentFiles.annotations",
-                      sampleIndex = c(1, 2), withIndex = c(1, 1))
-
-library(Spectra)
-## import the data and add it to the mse object
-spectra(mse) <- Spectra(fls, backend = MsBackendMzR())
-
 ## define the quality metrics to be calculated
 metrics <- c("areaUnderTic", "rtDuration", "msSignal10xChange")
 
@@ -37,7 +11,7 @@ metrics <- c("areaUnderTic", "rtDuration", "msSignal10xChange")
 ## additional parameters passed to the quality metrics functions
 ## (MSLevel is an argument of areaUnderTIC and msSignal10XChange,
 ## relativeTo is an argument of msSignal10XChange)
-qc <- calculateMetricsFromMsExperiment(msexp = mse, metrics = metrics, 
+qc <- calculateMetricsFromSpectra(spectra = spectra, metrics = metrics, 
     msLevel = 1, relativeTo = "Q1", change = "jump")
 rownames(qc) <- c("Sample 1", "Sample 2")
 
