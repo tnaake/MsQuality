b2e2c3474046a66b98efeca71c45b746a30a2f7c
Thomas Naake <thomasnaake@googlemail.com>
2026-03-02 13:16:59 +0100

    Update test_function_calculateMetrics_from_Spectra.R

--- tests/testthat/test_function_calculateMetrics_from_Spectra.R
+++ tests/testthat/test_function_calculateMetrics_from_Spectra.R
@@ -13,8 +13,10 @@ spectra <- Spectra(sciex_file)
 
 ## obtain the file paths
 dO <- unique(spectra$dataOrigin)
-dO_replaced <- gsub("[\\\\]", "/", dO)
-dO_replaced <- gsub("[\\]", "/", dO_replaced)
+#dO_replaced <- gsub("[\\\\]", "/", dO)
+#dO_replaced <- gsub("[\\]", "/", dO_replaced)
+dO_replaced <- sub(".*ExperimentHub[/\\\\]([^/\\\\]+)$", "\\1", dO)
+stop(dO_replaced)
 dO_cut <- lapply(strsplit(dO_replaced, split = "ExperimentHub/"), "[", 2) |>
     unlist()
 
